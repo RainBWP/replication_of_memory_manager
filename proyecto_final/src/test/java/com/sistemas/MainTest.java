8c870e15e9833ce46adfc3e9b1a39e274f348ad5
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-28 21:07:06 -0600

    Añade pruebas unitarias para la clase Main, incluyendo métodos para verificar la conversión de direcciones virtuales a físicas y la gestión de páginas en memoria.

---
+++ proyecto_final/src/test/java/com/sistemas/MainTest.java
@@ -0,0 +1,47 @@
+package com.sistemas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.util.logging.Logger;
+
+public class MainTest {
+    @Test
+    public void testObtenerPaginaDeVirtual() {
+        int pagina = Memoria_Traductor.obtener_pagina_de_virtual(185, 32);
+        assertEquals(5, pagina);
+    }
+    
+    @Test
+    public void testConvertirVirtualAFisica() {
+        Memoria_Fisica memoria_fisica = new Memoria_Fisica(1024);
+        memoria_fisica.addMemoria_con_pagina(5, 168);
+        int direccion_fisica = Memoria_Traductor.convertir_virtual_a_fisica(185, 
+                                                8, 
+                                                32, 
+                                                memoria_fisica);
+        
+        assertEquals(memoria_fisica.getMemoria_con_pagina(5), 168);
+        assertEquals(25, direccion_fisica);
+    }
+
+    @Test
+    public void testPaginas() {
+        Paginas pagina = new Paginas(168, 8);
+        assertEquals(168, pagina.getMemoria());
+        assertEquals(true, pagina.getReferencia());
+        assertEquals(false, pagina.getModificacion());
+        assertEquals(true, pagina.getPermiso());
+        assertEquals(false, pagina.getCache());
+        assertEquals(true, pagina.getPresenteAusente());
+        assertEquals(0, pagina.getFrame());
+
+        pagina = new Paginas(235, 8);
+        assertEquals(235, pagina.getMemoria());
+        assertEquals(true, pagina.getReferencia());
+        assertEquals(true, pagina.getModificacion());
+        assertEquals(true, pagina.getPermiso());
+        assertEquals(false, pagina.getCache());
+        assertEquals(true, pagina.getPresenteAusente());
+        assertEquals(3, pagina.getFrame());
+    }
+}
