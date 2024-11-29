79ea71778014313d969c0f5f93ff174e37ed6e39
lemisora <leonardo.michel.dom@gmail.com>
2024-11-28 22:42:26 -0600

    Se corrige el método para obtener el valor del desplazamiento - Aún faltan detalles por corregir para que se retorne el valor de dirección correcta

--- proyecto_final/src/main/java/com/sistemas/Memoria_Traductor.java
+++ proyecto_final/src/main/java/com/sistemas/Memoria_Traductor.java
@@ -13,16 +13,29 @@ public class Memoria_Traductor {
                                                 int tamano_de_marco, 
                                                 int tamano_de_pagina, 
                                                 Memoria_Fisica memoria_fisica) {
-        
-        int bits_de_desplazamiento = Integer.bitCount(tamano_de_pagina - 1); // obtenemos el desplazamiento
-        int mascara = direccion_virtual ^ Integer.MAX_VALUE; // obtenemos la mascara
-        int desplazamiento = direccion_virtual & mascara; // obtenemos el desplazamiento
+
+        int direccion_virtual_temporal = direccion_virtual; //Se guarda el valor de dirección virtual en una variable auxiliar
+        int bits_de_desplazamiento = Integer.bitCount(tamano_de_pagina - 1); // obtenemos los bits para desplazamiento
+
+        //System.out.println("Valor de bits de desplazamiento: "+bits_de_desplazamiento);
+
+        //Se obtiene el número de páginas haciendo corrimiento de bits
+        direccion_virtual_temporal >>= bits_de_desplazamiento;
+
+        //Procederemos a obtener el valor del desplazamiento
+        direccion_virtual_temporal <<= bits_de_desplazamiento;  //Se regresan los bits de desplazamiento a su lugar
+
+        int mascara = direccion_virtual_temporal ^ Integer.MAX_VALUE; //Se crea una máscara con el número mayor para Int 11111...
+        //System.out.println("Valor de mascara: "+mascara);
+        int desplazamiento = direccion_virtual & mascara; //Se obtiene el valor de desplazamiento con enmascaramiento al valor original de d.v.
+        //System.out.println("Valor de desplazamiento: "+desplazamiento);
 
         int memoria_fisica_valor = memoria_fisica.getMemoria_con_pagina(obtener_pagina_de_virtual(direccion_virtual, tamano_de_pagina)); // obtenemos el numero de marco
         Paginas pagina = new Paginas(memoria_fisica_valor, tamano_de_marco); // creamos una nueva pagina
 
         int virtual = pagina.getFrame() << bits_de_desplazamiento; // obtenemos la direccion virtual
         virtual |= desplazamiento; // obtenemos la direccion virtual
+        System.out.println("DV: " + virtual);
         return virtual;
     }
 
