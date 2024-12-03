74a570f219bf7efe09584dc9ced79b9058ab70d2
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-12-02 23:47:36 -0600

    Simplificacion en manejo de funciones y variables, se guarda el desplazamiento en una variable de menor tamano

--- proyecto_final/src/main/java/com/sistemas/Paginas.java
+++ proyecto_final/src/main/java/com/sistemas/Paginas.java
@@ -2,24 +2,11 @@ package com.sistemas;
 
 public class Paginas {
     private final int memoria; // Memoria física
-    private final boolean bit_de_referencia; // Bit de referencia
-    private final boolean bit_de_modificacion; // Bit de modificación
-    private final boolean bit_de_permiso; // Bit de permiso
-    private final boolean bit_de_cache; // Bit de cache
-    private final boolean bit_de_presente_ausente; // Bit de presente/ausente
-    private final int frame; // Frame
+    private final byte desplazamiento;
 
     public Paginas(int memoria, int numero_de_marco) {
-        this.memoria = memoria;
-        
-        int desplazamiento = Integer.bitCount(numero_de_marco - 1);
-
-        this.bit_de_referencia = ((memoria >> (desplazamiento + 4)) & 1) == 1;
-        this.bit_de_modificacion = ((memoria >> (desplazamiento + 3)) & 1) == 1;
-        this.bit_de_permiso = ((memoria >> (desplazamiento + 2)) & 1) == 1;
-        this.bit_de_cache = ((memoria >> (desplazamiento + 1)) & 1) == 1;
-        this.bit_de_presente_ausente = ((memoria >> desplazamiento) & 1) == 1;
-        this.frame = memoria & ((1 << desplazamiento) - 1);
+        this.memoria = memoria;    
+        this.desplazamiento = (byte) Integer.bitCount(numero_de_marco - 1);
     }
 
     public int getMemoria() {
@@ -27,27 +14,27 @@ public class Paginas {
     }
 
     public boolean getReferencia() {
-        return bit_de_referencia;
+        return ((memoria >> (desplazamiento + 4)) & 1) == 1;
     }
 
     public boolean getModificacion() {
-        return bit_de_modificacion;
+        return ((memoria >> (desplazamiento + 3)) & 1) == 1;
     }
 
     public boolean getPermiso() {
-        return bit_de_permiso;
+        return ((memoria >> (desplazamiento + 2)) & 1) == 1;
     }
 
     public boolean getCache() {
-        return bit_de_cache;
+        return ((memoria >> (desplazamiento + 1)) & 1) == 1;
     }
 
     public boolean getPresenteAusente() {
-        return bit_de_presente_ausente;
+        return ((memoria >> desplazamiento) & 1) == 1;
     }
 
     public int getFrame() {
-        return frame;
+        return memoria & ((1 << desplazamiento) - 1);
     }
 
 }
