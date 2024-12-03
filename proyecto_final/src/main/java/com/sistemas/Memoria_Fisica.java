4187c15755eb7cb013894506d2d0acc28b0c1563
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-12-03 00:03:27 -0600

    Se agrega el tamaño de marco a la clase Memoria_Fisica y se ajusta el constructor. Se implementan métodos para obtener el tamaño de marco y se corrige el manejo del porcentaje de uso.

--- proyecto_final/src/main/java/com/sistemas/Memoria_Fisica.java
+++ proyecto_final/src/main/java/com/sistemas/Memoria_Fisica.java
@@ -2,15 +2,25 @@ package com.sistemas;
 
 public class Memoria_Fisica {
     private final int tamano_de_memoria; // Tamaño de la memoria física - Se Recibira 1024 bytes por ejemplo
-    private int[] memoria; // Memoria física - Se guardara en un arreglo de 1024 bytes
+    private final int[] memoria; // Memoria física - Se guardara en un arreglo de 1024 bytes
+    private final short tamano_de_marco; // Tamaño de los marcos de la memoria física - Se Recibira 32 bytes por ejemplo
     private int porcentaje_de_uso; // Porcentaje de uso de la memoria física - Este porcentaje no es de 0-100, sino de 0-1024
-
-    public Memoria_Fisica(int tamano_de_memoria) {
+    
+    public Memoria_Fisica(int tamano_de_memoria, int tamano_de_marco) {
         this.tamano_de_memoria = tamano_de_memoria;
         this.memoria = new int[tamano_de_memoria];
+        this.tamano_de_marco = (short) tamano_de_marco;
         for (int i = 0; i < tamano_de_memoria; i++) {
             memoria[i] = 0;
         }
+    }
+
+    public short getTamanoDeMarco() {
+        return tamano_de_marco;
+    }
+
+    public byte getTamanoDeMarco_bytes() {
+        return (byte) Integer.bitCount(tamano_de_marco - 1);
     }
 
     public int getTamanoDeMemoria() {
@@ -28,9 +38,13 @@ public class Memoria_Fisica {
     public boolean addMemoria_con_pagina(int pagina, int dato) {
         if ((pagina < tamano_de_memoria) & (memoria[pagina] == 0)) {
             memoria[pagina] = dato;
-            porcentaje_de_uso++;
+            if (dato == 0) { // Si el dato es 0, se decrementa el porcentaje de uso ya que se esta liberando memoria
+                porcentaje_de_uso--;
+                
+            } else {
+                porcentaje_de_uso++;
+            }
             return true;
-
         }
         else {
             return false;
@@ -47,7 +61,7 @@ public class Memoria_Fisica {
         }
     }
 
-    public int getPorcentajeDeUso() {
+    public float getPorcentajeDeUso() { // Regresa el porcentaje 1.0 - 0.0
         return porcentaje_de_uso/tamano_de_memoria;
     }
 
