a9d0b2668c3def4bcf40d11932973b6756798b7a
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-12-03 01:21:13 -0600

    Agregar método para ejecutar procesos y manejar direcciones de memoria

--- proyecto_final/src/main/java/com/sistemas/Proceso.java
+++ proyecto_final/src/main/java/com/sistemas/Proceso.java
@@ -42,4 +42,50 @@ public class Proceso {
         return lastReference;
     }
 
+    public void ejecutar(Memoria_Virtual memoriaVirtual, Memoria_Fisica memoriaFisica) {
+        for (int referencia : referencias) {
+            try {
+                // Simula 1 segundo por referencia
+
+                // Actualiza la dirección virtual
+                // 
+
+                // Obtiene la página correspondiente
+                Paginas pagina = new Paginas(direccion_virtual+referencia, memoriaFisica.getTamanoDeMarco());
+
+                // Maneja el fallo de página si la página no está en memoria física
+                if (!pagina.getPresenteAusente()) {
+                    System.out.println("Fallo de página en proceso " + nombre_de_proceso);
+                    //memoriaFisica.fallo_de_pagina(pagina);
+                    continue; // quitar al agregar el manejo de fallo de página
+                }
+
+                // Calcula la dirección física
+                int direccion_fisica = Memoria_Traductor.convertir_virtual_a_fisica(direccion_virtual+referencia, 
+                                                memoriaFisica.getTamanoDeMarco(), 
+                                                memoriaVirtual.getTamanoDePagina(), 
+                                                memoriaFisica);
+
+                // Muestra la información requerida
+                System.out.println("Proceso: " + nombre_de_proceso);
+                System.out.println("Dirección virtual: " + direccion_virtual);
+                System.out.println("Dirección física: " + direccion_fisica);
+                System.out.println("Número de marco: " + pagina.getFrame());
+                System.out.println("Bits de control:");
+                System.out.println("  Referencia: " + pagina.getReferencia());
+                System.out.println("  Modificación: " + pagina.getModificacion());
+                System.out.println("  Permiso: " + pagina.getPermiso());
+                System.out.println("  Caché: " + pagina.getCache());
+                System.out.println("  Presente/Ausente: " + pagina.getPresenteAusente());
+                System.out.println("-----------------------------------");
+                
+                Thread.sleep(1000);
+
+
+            } catch (InterruptedException e) {
+                System.err.println("Thread was interrupted: " + e.getMessage());
+            }
+        }
+    }
+
 }
