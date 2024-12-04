1bbacd4c87a7056488080c4fd736f8c935686855
lemisora <leonardo.michel.dom@gmail.com>
2024-12-04 16:00:23 -0600

    Se corrigen detalles para calcular atributos de entrada de tabla de páginas

--- proyecto_final/src/main/java/com/sistemas/Paginas.java
+++ proyecto_final/src/main/java/com/sistemas/Paginas.java
@@ -2,11 +2,12 @@ package com.sistemas;
 
 public class Paginas {
     private final int entrada_tabla_pagina; // Memoria física
-    private final byte desplazamiento;
+    private final byte bits_desplazamiento;
 
-    public Paginas(int entrada, int desplazamiento) {
+    public Paginas(int entrada, int numero_marcos) {
         this.entrada_tabla_pagina = entrada;
-        this.desplazamiento = (byte) desplazamiento;
+        this.bits_desplazamiento = (byte) Integer.bitCount(numero_marcos - 1);
+//        System.out.println("Valor de bits de desplazamiento para obtener número de marco: " + bits_desplazamiento);
     }
 
     public int getEntrada() {
@@ -14,27 +15,27 @@ public class Paginas {
     }
 
     public boolean getReferencia() {
-        return ((entrada_tabla_pagina >> (desplazamiento + 4)) & 1) == 1;
+        return ((entrada_tabla_pagina >> (bits_desplazamiento + 4)) & 1) == 1;
     }
 
     public boolean getModificacion() {
-        return ((entrada_tabla_pagina >> (desplazamiento + 3)) & 1) == 1;
+        return ((entrada_tabla_pagina >> (bits_desplazamiento + 3)) & 1) == 1;
     }
 
     public boolean getPermiso() {
-        return ((entrada_tabla_pagina >> (desplazamiento + 2)) & 1) == 1;
+        return ((entrada_tabla_pagina >> (bits_desplazamiento + 2)) & 1) == 1;
     }
 
     public boolean getCache() {
-        return ((entrada_tabla_pagina >> (desplazamiento + 1)) & 1) == 1;
+        return ((entrada_tabla_pagina >> (bits_desplazamiento + 1)) & 1) == 1;
     }
 
     public boolean getPresenteAusente() {
-        return ((entrada_tabla_pagina >> desplazamiento) & 1) == 1;
+        return ((entrada_tabla_pagina >> bits_desplazamiento) & 1) == 1;
     }
 
     public int getFrame() {
-        return entrada_tabla_pagina & ((1 << desplazamiento) - 1);
+        return entrada_tabla_pagina & ((1 << bits_desplazamiento)-1);
     }
 
 }
