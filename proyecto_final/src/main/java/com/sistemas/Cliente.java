4197a49362b8065d89827f63a85d860d636330de
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-12-04 01:16:14 -0600

    Agregar clases Cliente y Servidor

---
+++ proyecto_final/src/main/java/com/sistemas/Cliente.java
@@ -0,0 +1,11 @@
+package com.sistemas;
+
+public class Cliente {
+    private final String direccion_ip;
+
+    public Cliente(String direccion_ip) {
+        this.direccion_ip = direccion_ip;
+    }
+
+    
+}
---
+++ proyecto_final/src/main/java/com/sistemas/Servidor.java
@@ -0,0 +1,9 @@
+package com.sistemas;
+
+public class Servidor {
+    private final String direccion_ip;
+
+    public Servidor(String direccion_ip) {
+        this.direccion_ip = direccion_ip;
+    }
+}
